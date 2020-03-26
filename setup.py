40c9193baa9224f0f6893de4e59cb3d56882e48f
Geir Freysson <geir@transmit.is>
2020-03-26 11:52:38 +0000

    Add download url and classifiers to cfg.

--- setup.py
+++ setup.py
@@ -7,9 +7,14 @@ setuptools.setup(
     version="0.1",
     licence='MIT',
     url="https://github.com/datasmoothie/datasmoothie-client-python",
+    download_url="https://github.com/datasmoothie/datasmoothie-client-python/archive/v0.1.tar.gz",
     author="Geir Freysson",
     author_email="geir@datasmoothie.com",
     description="Python wrapper for the v2  of the Datasmoothie API.",
     keywords=['surveys', 'market research', 'weighting', 'significance tests'],
-    classifiers=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
