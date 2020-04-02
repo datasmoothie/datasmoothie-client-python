be5f06a385ca19afec5fc1fc8f352a5d0e41eedb
Geir Freysson <geir@transmit.is>
2020-04-02 16:56:56 +0100

    Update pip package config to v0.2.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import setuptools
 setuptools.setup(
     name="datasmoothie",
     packages=['datasmoothie'],
-    version="0.1",
+    version="0.2",
     licence='MIT',
     url="https://github.com/datasmoothie/datasmoothie-client-python",
-    download_url="https://github.com/datasmoothie/datasmoothie-client-python/archive/v0.1.tar.gz",
+    download_url="https://github.com/datasmoothie/datasmoothie-client-python/archive/v0.2.tar.gz",
     author="Geir Freysson",
     author_email="geir@datasmoothie.com",
-    description="Python wrapper for the v2  of the Datasmoothie API.",
+    description="Python wrapper for v2 of the Datasmoothie API.",
     keywords=['surveys', 'market research', 'weighting', 'significance tests'],
     classifiers=[
         "Programming Language :: Python :: 3",
