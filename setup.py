01999cb73f8ae67ae103f9766225349e2bf4575c
Geir Freysson <geir@transmit.is>
2020-04-22 18:02:30 +0100

    Correct importing of chart templates.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import setuptools
 
 setuptools.setup(
     name="datasmoothie",
-    packages=['datasmoothie'],
+    packages=setuptools.find_packages(),
     extras_require={':python_version<"3.7"': ['importlib-resources']},
-    version="0.8",
+    version="0.9",
     license='MIT',
     include_package_data=True,
     url="https://github.com/datasmoothie/datasmoothie-client-python",
-    download_url="https://github.com/datasmoothie/datasmoothie-client-python/archive/v0.8.tar.gz",
+    download_url="https://github.com/datasmoothie/datasmoothie-client-python/archive/v0.9.tar.gz",
     author="Geir Freysson",
     author_email="geir@datasmoothie.com",
     description="Python wrapper for v2 of the Datasmoothie API.",
